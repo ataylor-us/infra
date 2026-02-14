503d8aaa4cf6ca10310fc891ffbc6b0dc2e7e8bd
Alex Taylor <ataylor@posteo.us>
2026-02-14 10:54:53 -0500

    feat(dev,system): added root ssh key generation rescue block, started opentofu vm provisioning for testing

---
+++ dev/workstation/main.tf
@@ -0,0 +1,68 @@
+resource "libvirt_volume" "fedora_base" {
+  name   = "fedora43-base.qcow2"
+  pool   = var.volume_pool
+  source = var.fedora_image_source
+  format = "qcow2"
+}
+
+resource "libvirt_volume" "fedora_disk" {
+  name           = "${var.vm_name}.qcow2"
+  pool           = var.volume_pool
+  base_volume_id = libvirt_volume.fedora_base.id
+  size           = var.vm_disk_size
+  format         = "qcow2"
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit" {
+  name = "${var.vm_name}-cloudinit.iso"
+  pool = var.volume_pool
+
+  user_data = templatefile("${path.module}/user-data.tpl", {
+    hostname       = var.vm_name
+    ssh_public_key = var.ssh_public_key
+  })
+
+  meta_data = templatefile("${path.module}/meta-data.tpl", {
+    instance_id    = var.vm_name
+    local_hostname = var.vm_name
+  })
+
+  network_config = templatefile("${path.module}/network-config.tpl", {
+    ip_address = "${var.subnet}.${var.vm_ip_suffix}"
+    gateway    = "${var.subnet}.1"
+  })
+}
+
+resource "libvirt_domain" "workstation_staging" {
+  name       = var.vm_name
+  memory     = var.vm_memory
+  vcpu       = var.vm_vcpu
+  qemu_agent = false
+
+  network_interface {
+    network_name   = "default"
+    wait_for_lease = false
+  }
+
+  disk {
+    volume_id = libvirt_volume.fedora_disk.id
+  }
+
+  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "0"
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  graphics {
+    type        = "vnc"
+    listen_type = "address"
+    autoport    = true
+  }
+}
---
+++ dev/workstation/network-config.tpl
@@ -0,0 +1,9 @@
+version: 2
+ethernets:
+  ens3:
+    addresses:
+      - ${ip_address}/24
+    gateway4: ${gateway}
+    nameservers:
+      addresses:
+        - ${gateway}
